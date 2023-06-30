3a66a13a38df9dd74ee639451d26a6a4ef8b6bee
luthfimauludy <putra.luthfi@gmail.com>
2023-06-30 22:15:11 +0700

    feat: add persist gate with redux persist

--- App.jsx
+++ App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Main from './src/screens/Main';
-import store from './src/redux/store';
+import {store, persistor} from './src/redux/store';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/es/integration/react';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Main />
+      <PersistGate persistor={persistor}>
+        <Main />
+      </PersistGate>
     </Provider>
   );
 };
