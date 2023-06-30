f032e20288b8eaffc12f228c1351869585dbd8a4
luthfimauludy <putra.luthfi@gmail.com>
2023-06-30 23:00:52 +0700

    feat: use selector for token with redux

--- src/screens/Main.jsx
+++ src/screens/Main.jsx
@@ -5,28 +5,36 @@ import Register from './auth/Register';
 import Login from './auth/Login';
 import ForgotPassword from './auth/ForgotPassword';
 import ResetPassword from './auth/ResetPassword';
-// import Home from './Home';
-// import Profile from './Profile';
-// import Event from './Event';
+import {useSelector} from 'react-redux';
+import Home from './Home';
+import Profile from './Profile';
+import Event from './Event';
+import Booking from './Booking';
 
 const AuthStack = createNativeStackNavigator();
-// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const Main = () => {
+  const token = useSelector(state => state.auth.token);
   return (
     <NavigationContainer>
-      <AuthStack.Navigator
-        screenOptions={{headerShadowVisible: false, headerTitle: ''}}>
-        <AuthStack.Screen name="Register" component={Register} />
-        <AuthStack.Screen name="Login" component={Login} />
-        <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
-        <AuthStack.Screen name="ResetPassword" component={ResetPassword} />
-      </AuthStack.Navigator>
-      {/* <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Profile" component={Profile} />
-        <Stack.Screen name="Event" component={Event} />
-      </Stack.Navigator> */}
+      {!token && (
+        <AuthStack.Navigator
+          screenOptions={{headerShadowVisible: false, headerTitle: ''}}>
+          <AuthStack.Screen name="Register" component={Register} />
+          <AuthStack.Screen name="Login" component={Login} />
+          <AuthStack.Screen name="ForgotPassword" component={ForgotPassword} />
+          <AuthStack.Screen name="ResetPassword" component={ResetPassword} />
+        </AuthStack.Navigator>
+      )}
+      {token && (
+        <Stack.Navigator screenOptions={{headerShadowVisible: false}}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Profile" component={Profile} />
+          <Stack.Screen name="Event" component={Event} />
+          <Stack.Screen name="Booking" component={Booking} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
