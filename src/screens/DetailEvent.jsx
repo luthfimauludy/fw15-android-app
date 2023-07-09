16da604916fbbb57b01ddf329e062cb86a74e301
luthfimauludy <putra.luthfi@gmail.com>
2023-07-09 10:13:00 +0700

    update: add icon wishlist and back button

--- src/screens/DetailEvent.jsx
+++ src/screens/DetailEvent.jsx
@@ -5,32 +5,114 @@ import {
   StyleSheet,
   Image,
   ImageBackground,
+  TouchableOpacity,
 } from 'react-native';
 import React from 'react';
-import Icon from 'react-native-vector-icons/Feather';
-import {Link} from '@react-navigation/native';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {Link, useFocusEffect} from '@react-navigation/native';
 import Button from '../components/Button';
+import {useSelector} from 'react-redux';
+import http from '../helpers/http';
+import moment from 'moment';
 
 const galery = require('../assets/img/galery.jpg');
 const maps = require('../assets/img/maps.png');
-const DetailEvent = () => {
+const DetailEvent = ({route, navigation}) => {
+  const {id} = route.params;
+  const token = useSelector(state => state.auth.token);
+  const [eventDetail, setEventDetail] = React.useState({});
+  const [wishlistBtn, setWishlistBtn] = React.useState(false);
+
+  React.useEffect(() => {
+    const getEventData = async () => {
+      const {data} = await http().get(`/events/${id}`);
+      setEventDetail(data.results);
+    };
+    if (id) {
+      getEventData(id);
+    }
+  }, [id]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const eventId = {eventId: id};
+      const qs = new URLSearchParams(eventId).toString();
+      const fetchData = async () => {
+        const {data} = await http(token).get(`/wishlist/check?${qs}`);
+        const btnStatus = data.results;
+        if (btnStatus) {
+          setWishlistBtn(true);
+        } else {
+          setWishlistBtn(false);
+        }
+      };
+      fetchData();
+    }, [token, id]),
+  );
+
+  const addRemoveWishlist = async () => {
+    try {
+      const eventId = {eventId: id};
+      const qs = new URLSearchParams(eventId).toString();
+      const {data} = await http(token).post('/wishlists', qs);
+      console.log(data);
+      if (wishlistBtn) {
+        setWishlistBtn(false);
+      } else {
+        setWishlistBtn(true);
+      }
+    } catch (err) {
+      const message = err?.response?.data?.message;
+      if (message) {
+        console.log(message);
+      }
+    }
+  };
+
+  const handlePressEvent = eventId => {
+    navigation.navigate('Booking', {eventId});
+  };
+
   return (
     <ScrollView style={styles.wrapper}>
       <ImageBackground source={galery} style={styles.imgBackground}>
+        <View style={styles.flexRow}>
+          <View>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+              <Feather name="arrow-left" size={25} color="#FFF" />
+            </TouchableOpacity>
+          </View>
+          <View>
+            <TouchableOpacity onPress={addRemoveWishlist}>
+              {wishlistBtn === true ? (
+                <FontAwesome name="heart" size={25} color="red" />
+              ) : (
+                <FontAwesome name="heart-o" size={25} color="#FFF" />
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
         <View style={styles.paddingTop50}>
           <View style={styles.eventBanner}>
             <View>
               <Link to="/Booking" style={styles.eventTitle}>
-                Sights & Sounds Exhibition
+                {eventDetail?.title}
               </Link>
             </View>
             <View style={styles.eventInfo}>
-              <Icon name="map-pin" size={25} color="#FC1055" />
-              <Text style={styles.whiteText}>Jakarta, Indonesia</Text>
+              <Feather name="map-pin" size={25} color="#FC1055" />
+              <Text style={styles.whiteText}>
+                {eventDetail?.location}, Indonesia
+              </Text>
             </View>
             <View style={styles.eventInfo}>
-              <Icon name="clock" size={25} color="#FC1055" />
-              <Text style={styles.whiteText}>Wed, 15 Nov, 4:00 PM</Text>
+              <Feather name="clock" size={25} color="#FC1055" />
+              <Text style={styles.whiteText}>
+                {moment(eventDetail.date).format('LLLL').slice(0, 3)}
+                {', '}
+                {moment(eventDetail.date).format('LLL')}
+              </Text>
             </View>
             <View>
               <Text style={styles.whiteText}>Attendees</Text>
@@ -49,7 +131,9 @@ const DetailEvent = () => {
               <Image source={maps} />
             </View>
             <View>
-              <Button>Buy Tickets</Button>
+              <Button onPress={() => handlePressEvent(eventDetail.id)}>
+                Buy Tickets
+              </Button>
             </View>
           </View>
         </View>
@@ -64,6 +148,12 @@ const styles = StyleSheet.create({
   },
   paddingTop50: {
     paddingTop: 50,
+  },
+  flexRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    paddingTop: 20,
   },
   imgBackground: {
     flex: 1,
@@ -117,6 +207,30 @@ const styles = StyleSheet.create({
     fontSize: 12,
     lineHeight: 20,
     letterSpacing: 1,
+  },
+  btnContain: {
+    width: '100%',
+    position: 'absolute',
+    paddingHorizontal: 20,
+    bottom: 30,
+  },
+  btnTouch: {
+    backgroundColor: '#4c3f91',
+    width: '100%',
+    height: 55,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 3},
+    shadowOpacity: 0.7,
+    shadowRadius: 10,
+    elevation: 4,
+  },
+  btnText: {
+    color: 'white',
+    fontSize: 16,
+    fontFamily: 'Poppins-SemiBold',
   },
 });
 
