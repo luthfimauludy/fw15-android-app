4bf7f40b1fd95f34f7e162f119237bc32c1aa75a
luthfimauludy <putra.luthfi@gmail.com>
2023-07-10 02:07:05 +0700

    fix: edit profile integration

--- src/screens/EditProfile.jsx
+++ src/screens/EditProfile.jsx
@@ -4,8 +4,6 @@ import {
   StyleSheet,
   ScrollView,
   TouchableOpacity,
-  Image,
-  Platform,
 } from 'react-native';
 import React from 'react';
 import Input from '../components/Input';
@@ -17,25 +15,27 @@ import {useSelector} from 'react-redux';
 import http from '../helpers/http';
 import Feather from 'react-native-vector-icons/Feather';
 import moment from 'moment';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import ImageTemplate from '../components/ImageTemplate';
+import {defaultPic} from '../assets';
 
-const picture = require('../assets/img/default-picture.jpg');
+// const defaultPic = require('../assets/img/default-picture.jpg');
 
 const EditProfile = () => {
+  const token = useSelector(state => state.auth.token);
   const [editFullName, setEditFullName] = React.useState(false);
   const [editEmail, setEditEmail] = React.useState(false);
   const [gender, setGender] = React.useState('0');
   const [editPhoneNumber, setEditPhoneNumber] = React.useState(false);
-  const [editProfession, setEditProfession] = React.useState(false);
-  const [editNasionality, setEditNasionality] = React.useState(false);
+  const [editProfession, setEditProfession] = React.useState('');
+  const [editNasionality, setEditNasionality] = React.useState('');
   const [editBirthDate, setEditBirthDate] = React.useState(false);
   const [open, setOpen] = React.useState(false);
   const [openSelect, setOpenSelect] = React.useState(false);
   const [professionValue, setProfessionValue] = React.useState(null);
   const [nasionalityValue, setNasionalityValue] = React.useState(null);
-  const token = useSelector(state => state.auth.token);
-  const [picture, setPicture] = React.useState(null);
   const [profile, setProfile] = React.useState({});
-  const [successMessage, setSuccessMessage] = React.useState('');
+  const [fileResponse, setFileResponse] = React.useState([]);
   const [profession, setProfession] = React.useState([
     {label: 'Web Developer', value: 'webdeveloper'},
     {label: 'Architect', value: 'architect'},
@@ -50,19 +50,11 @@ const EditProfile = () => {
   ]);
 
   React.useEffect(() => {
-    async function getProfileUser() {
-      try {
-        const {data} = await http(token).get('/profile');
-        setProfile(data.results);
-        console(data.results);
-      } catch (error) {
-        const message = error?.response?.data?.message;
-        if (message) {
-          console.log(message);
-        }
-      }
-    }
-    getProfileUser();
+    const getProfile = async () => {
+      const {data} = await http(token).get('/profile');
+      setProfile(data.results);
+    };
+    getProfile();
   }, [token]);
 
   const handleRadioPress = value => {
@@ -70,86 +62,145 @@ const EditProfile = () => {
   };
 
   const doEditProfile = async values => {
-    console.log(
-      values.fullName,
-      values.email,
-      values.phoneNumber,
-      professionValue,
-      nasionalityValue,
-      picture,
-    );
-    try {
-      const form = new FormData();
-      Object.keys(values).forEach(key => {
-        if (values[key]) {
-          if (key === 'birthDate') {
-            form.append(key, moment(values[key]).format('YYYY/MM/DD'));
-          } else {
-            form.append(key, values[key]);
-          }
-        }
-      });
-      form.append('fullName', values.fullName);
-      form.append('email', values.email);
-      form.append('phoneNumber', values.phoneNumber);
-      form.append('profession', professionValue);
-      form.append('nasionality', nasionalityValue);
-
-      if (picture) {
-        form.append('picture', {
-          name: picture.fileName,
-          type: picture.type,
-          uri:
-            Platform.OS === 'android'
-              ? picture.uri.replace('file://', ' ')
-              : picture.uri,
-        });
+    const form = new FormData();
+    Object.keys(values).forEach(key => {
+      if (values[key]) {
+        form.append(key, values[key]);
       }
+    });
 
-      if (token) {
-        const {data} = await http(token).post('/profile', form, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-          },
-        });
-        setSuccessMessage(data.message);
-        console.log(data);
-      }
-    } catch (err) {
-      const message = err?.response?.data?.message;
-      console.log(message);
+    const fileImage = {
+      uri: fileResponse,
+      type: 'image/jpeg',
+      name: 'image' + '-' + Date.now() + '.jpg',
+    };
+
+    if (fileResponse.length > 1) {
+      form.append('picture', fileImage);
     }
+
+    const getProfile = async () => {
+      const {data} = await http(token).get('/profile');
+      setProfile(data.results);
+    };
+
+    try {
+      const {data} = await http(token).patch('/profile', form, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      setProfile(data.results);
+    } catch (err) {
+      console.warn(err);
+    }
+
+    setEditFullName(false);
+    setEditEmail(false);
+    setEditPhoneNumber(false);
+    setGender(false);
+    getProfile();
+    setFileResponse([]);
   };
+
+  const handleDocumentSelection = React.useCallback(async () => {
+    try {
+      const response = await launchImageLibrary({
+        presentationStyle: 'fullScreen',
+      });
+
+      setFileResponse(response.assets[0].uri);
+    } catch (err) {
+      console.warn(err);
+    }
+  }, []);
+
+  const handleCameraSelection = React.useCallback(async () => {
+    try {
+      const response = await launchCamera({
+        presentationStyle: 'fullScreen',
+      });
+
+      setFileResponse(response.assets[0].uri);
+    } catch (err) {
+      console.warn(err);
+    }
+  }, []);
+
+  // try {
+  //   const form = new FormData();
+  //   Object.keys(values).forEach(key => {
+  //     if (values[key]) {
+  //       if (key === 'birthDate') {
+  //         form.append(key, moment(values[key]).format('YYYY/MM/DD'));
+  //       } else {
+  //         form.append(key, values[key]);
+  //       }
+  //     }
+  //   });
+  //   form.append('fullName', values.fullName);
+  //   form.append('email', values.email);
+  //   form.append('phoneNumber', values.phoneNumber);
+  //   form.append('profession', professionValue);
+  //   form.append('nasionality', nasionalityValue);
+
+  //   if (token) {
+  //     const {data} = await http(token).patch('/profile', form, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data',
+  //       },
+  //     });
+  //     setProfile(data.results);
+  //     setSuccessMessage(data.message);
+  //     console.log(data);
+  //   }
+  // } catch (err) {
+  //   const message = err?.response?.data?.message;
+  //   console.log(message);
+  // }
 
   return (
     <ScrollView style={style.container}>
       <View style={style.contEditProf}>
-        <View style={style.fotoCont}>
-          <View style={style.foto}>
-            <View style={style.fotoIcon}>
-              <Image style={style.picture} source={picture} />
-            </View>
-          </View>
-        </View>
         <Formik
           initialValues={{
-            fullName: '',
-            email: '',
-            phoneNumber: '',
+            fullName: profile?.fullName,
+            email: profile?.email,
+            phoneNumber: profile?.phoneNumber,
             gender: profile?.gender ? '1' : '0',
-            birthDate: '',
+            profession: profile?.profession,
+            nasionality: profile?.nasionality,
+            birthDate: profile?.birthDate,
           }}
           onSubmit={doEditProfile}
           enableReinitialize>
           {({handleBlur, handleChange, handleSubmit, values}) => (
             <>
-              <View style={style.nameCont}>
-                {successMessage && (
-                  <View>
-                    <Feather name="check" size={25} />
-                    <Text>{successMessage}</Text>
+              <View style={style.profileWrapper}>
+                <View style={style.photosContent}>
+                  <View style={style.photoIcons}>
+                    <ImageTemplate
+                      src={profile?.picture || null}
+                      defaultImg={defaultPic}
+                      style={style.IMGProfiles}
+                    />
                   </View>
-                )}
+                </View>
+                <View style={style.textGap20}>
+                  <TouchableOpacity
+                    style={style.btnLoadImg}
+                    onPress={handleDocumentSelection}>
+                    <Feather name="file-plus" size={25} color="black" />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={style.btnLoadImg}
+                    onPress={handleCameraSelection}>
+                    <Feather name="camera" size={25} color="black" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <View style={style.nameCont}>
                 <Text style={style.title}>Full Name</Text>
                 <View style={style.flexCont}>
                   {editFullName && (
@@ -381,34 +432,6 @@ const style = StyleSheet.create({
     padding: 30,
     gap: 20,
   },
-  foto: {
-    width: 137,
-    height: 137,
-    borderWidth: 5,
-    borderColor: '#61764B',
-    borderRadius: 70,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 30,
-  },
-  fotoIcon: {
-    width: 110,
-    height: 110,
-    backgroundColor: 'gray',
-    borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden',
-  },
-  fotoCont: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  picture: {
-    objectFit: 'cover',
-    width: '100%',
-    height: '100%',
-  },
   textInput: {
     fontSize: 16,
     padding: 14,
@@ -441,6 +464,48 @@ const style = StyleSheet.create({
   },
   textRed: {
     color: 'red',
+  },
+  profileWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 20,
+    gap: 10,
+  },
+  photosContent: {
+    width: 137,
+    height: 137,
+    borderWidth: 5,
+    borderColor: '#61764B',
+    borderRadius: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+  photoIcons: {
+    width: 115,
+    height: 115,
+    backgroundColor: 'gray',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  IMGProfiles: {
+    objectFit: 'cover',
+    width: '100%',
+    height: '100%',
+  },
+  textGap20: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 40,
+  },
+  btnLoadImg: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 5,
   },
 });
 
